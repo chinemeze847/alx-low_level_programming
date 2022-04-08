e6fa9e1cbc7a091e5eb791bd5ea119f95a5afca3
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-08 12:15:04 -0700

    task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - This is the entry point of my program 
+ *
+ * Description: This prints a message to the standard error output
+ *
+ * Return: This returns 1
+ *
+ */
+
+int main(void)
+{
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
