7b42d3e9457dbb534a765855f7094860bb1f4338
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-08 11:37:49 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - This function prints the string
+ *
+ * Description : with proper grammar, but the outcome is a piece of art
+ *
+ * Return :  This function returns 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
