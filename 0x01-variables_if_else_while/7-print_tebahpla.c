a9b78959d31f846b8749dbef23dd0a3d34575b5d
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-11 08:46:49 -0700

    task 7;

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
