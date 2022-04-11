24b77aa82e78d7e5761502e8823856ac8b557ee0
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-11 08:29:17 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet without e and q
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
