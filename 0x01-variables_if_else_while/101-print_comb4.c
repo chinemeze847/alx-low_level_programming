e75861c8a2d9bca1d7908215f90ffe08f305823b
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-11 09:42:12 -0700

    task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i, n, j = 0;
+
+	while (j < 10)
+	{
+		n = 0;
+
+		while (n < 10)
+		{
+			i = 0;
+
+			while (i < 10)
+			{
+				if (i != n && n != j && j < n && n < i)
+				{
+					putchar(j + '0');
+					putchar(n + '0');
+					putchar(i + '0');
+
+					if (i + n+ j != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				i++;
+			}
+			n++;
+	       	}
+	        j++;
+	}
+	putchar('\n');
+
+	return (0);
+}
