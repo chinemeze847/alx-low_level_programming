e320b868f36ac5dcb9b54857f81fb24bdaeae7f0
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-21 14:50:05 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+/**
+ * strlen - finds the length of a string
+ * @s: Input char
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0)
+	{
+		s++;
+		l++;
+	}
+
+	return (l);
+}
