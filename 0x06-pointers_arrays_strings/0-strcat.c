f9e398e13633da8d24a9a0172fa336887b263cc6
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-26 13:38:04 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	while (des[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
@@ -24,7 +24,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	des[i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
