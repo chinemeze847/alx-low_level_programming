ad087375eed0d40275332ca2e9b9a97959ceb60c
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-26 16:15:58 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,19 +15,23 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int length, j;
 
+	/* Loop through strings */
 	for (length = 0; *(s + length) != '\0'; length++)
 	{
+		/* Loop through string accept */
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
+			/*Check where the first character in string j appears */
+			/* in string c and immediately break out of the loop */
 			if (*(s + length) == *(accept + j))
 			{
 				break;
 			}
 		}
-
+                /* Check if there is no string accept to loop through */
 		if (! *(accept + j))
 			break;
 	}
-
+        /* Return the length where the first character appears */
 	return (length);
 }
