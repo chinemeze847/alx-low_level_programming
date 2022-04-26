96e3bd7916ad3504935bff0df5a5b6a297210b93
chinemeze847 <thankgodchinemeze@gmail.com>
2022-04-26 14:31:26 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isLower - Determines whether ASCII is lowercase
+ * @c: Character
+ *
+ * Return: 1 if true, o if false
+ */
+
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - Determines whether ASCII is a delimeter
+ * @c: Character 
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimeter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimeter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: Input string
+ *
+ * Return: String with capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+        int foundDelimiter = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimiter)
+		{
+			*s -= 32;
+			foundDelimiter = 0;
+		}
+		else
+		{
+			foundDelimiter = 0;
+		}
+		s++;
+	}
+        
+	return (ptr);
+}
