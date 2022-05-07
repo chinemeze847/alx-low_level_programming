168bd430eb8db32439489679acf11a4660047d57
chinemeze847 <thankgodchinemeze@gmail.com>
2022-05-06 19:05:33 -0700

    corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,34 +23,28 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (*(s1 + s1_length) != '\0')
 	{
 		s1_length++;
 	}
-
 	while (*(s2 + s2_length) != '\0')
 	{
 		s2_length++;
 	}
-
 	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
 
 	if (alloc_mem == NULL)
 	{
 		return ('\0');
 	}
-
 	for (i = 0; i < s1_length; i++)
 	{
 		alloc_mem[i] = *(s1 + i);
 	}
-
 	for (j = 0; j < s2_length; j++, i++)
 	{
 		alloc_mem[i] = *(s2 + j);
