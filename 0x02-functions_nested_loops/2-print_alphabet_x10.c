26b79838c4e4497843f4ed075e0483148d3a8c1a
chinemeze847 <thankgodchinemeze@gmail.com>
2022-05-07 05:51:21 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,7 +14,7 @@ void print_alphabet_x10(void)
 	count = 0;
 	while (count < 10)
 	{
-		for (alpahbet = 'a'; alphabet <= 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
