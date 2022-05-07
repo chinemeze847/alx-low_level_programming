208ede9592a1410e220d0201436575145f3489d1
chinemeze847 <thankgodchinemeze@gmail.com>
2022-05-06 18:57:28 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings
+ *
+ * @s1: The stringg to concatenate on
+ * @s2: The string to be concatenated to s1
+ *
+ * Return: If concatenation fails - NULL
+ * otherwise - a pointer to newly allocated space in memory
+ * containing the concatenated strings.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	char *alloc_mem;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (*(s1 + s1_length) != '\0')
+	{
+		s1_length++;
+	}
+
+	while (*(s2 + s2_length) != '\0')
+	{
+		s2_length++;
+	}
+
+	alloc_mem = malloc(1 +(s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+
+	if (alloc_mem == NULL)
+	{
+		return ('\0');
+	}
+
+	for (i = 0; i < s1_length; i++)
+	{
+		alloc_mem[i] = *(s1 + i);
+	}
+
+	for (j = 0; j < s2_length: j++, i++)
+	{
+		alloc_mem[i] = *(s2 + j);
+	}
+
+	return (alloc_mem);
+}
