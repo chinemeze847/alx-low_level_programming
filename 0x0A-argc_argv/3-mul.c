8247d2ac49cd087057d4f0a479b53b8af75500d9
chinemeze847 <thankgodchinemeze@gmail.com>
2022-05-07 16:57:49 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Program that adds two numbers
+ * @argc: number of arguments passed to the funcition
+ * @argv: argument vector of pointers to strings.
+ *
+ * Return: 0 (Success) or 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
