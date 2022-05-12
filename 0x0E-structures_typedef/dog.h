90213848d1d8c79f79341ff3b0603900f4974b9e
chinemeze847 <thankgodchinemeze@gmail.com>
2022-05-12 05:06:56 -0700

    Little poppies look nice

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Auth: Chinemeze ThankGod
+ * Desc:Header file to define new struct
+ */
+
+/**
+ * struct dog - It describe a dog
+ * @name: name of the doge
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: A dog struct
+ */
+struct dog
+{
+	char *name;
+	int   age;
+	char *owner;
+}
+
+#endif
